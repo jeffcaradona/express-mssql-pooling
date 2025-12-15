25181ad6abcca4d152311281f337e2ded3fd014a
jeffcaradona <jeffcaradona@gmail.com>
2025-12-15 07:33:07 -0600

    Update apiRouter.js

--- src/routes/apiRouter.js
+++ src/routes/apiRouter.js
@@ -13,7 +13,15 @@ apiRouter.get("/record-count", getRecordCount);
 import { getBadTest } from "../controllers/apiController.js";
 apiRouter.get("/failure-test", getBadTest);
 
+apiRouter.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
+  // render the error json
+  res.status(err.status || 500);
+  res.json({code:'', message:''})
+});
 
 
 
